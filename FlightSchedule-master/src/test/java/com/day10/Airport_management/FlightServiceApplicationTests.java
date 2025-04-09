58acb2dd7b18fd343799d92168a7702775f5f07a
venumadhav7777 <124698370+venumadhav7777@users.noreply.github.com>
2025-04-09 10:01:45 +0530

    Add files via upload

---
+++ FlightSchedule-master/src/test/java/com/day10/Airport_management/FlightServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.day10.Airport_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
